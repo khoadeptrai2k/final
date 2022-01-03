bd5eafe9bbf806bb8615ed7bb76d338f0d70b4b4
Huynh Mac Tu Khoa <huynhmactukhoa311@MacBook-Pro-cua-Huynh.local>
2022-01-03 18:01:22 +0700

    connect mongodb

---
+++ back-end/server.js
@@ -0,0 +1,37 @@
+require('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const cookieParser = require('cookie-parser')
+const fileUpload = require('express-fileupload')
+const path = require('path')
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+app.use(cookieParser())
+app.use(fileUpload({
+    useTempFiles: true
+}))
+
+// app.use('/', (req, res, next) => {
+//     res.json({msg: "hi"})
+// })
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => {
+    console.log('Server is running on port', PORT)
+})
+
+// connect to mongodb
+const URI = process.env.MONGODB_URL
+mongoose.connect(URI, {
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}, err => {
+    if(err) throw err;
+    console.log("Connected to mongodb")
+})
+
