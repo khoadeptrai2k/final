977a307f2637bd625020d3b5b77e46a0f165d99e
Huynh Mac Tu Khoa <huynhmactukhoa311@MacBook-Pro-cua-Huynh.local>
2022-02-12 23:54:44 +0700

    fixing and done Login

--- front-end/src/pages/login.js
+++ front-end/src/pages/login.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react'
 import { Link } from 'react-router-dom'
-
+import axios from 'axios'
+import { showErrMsg, showSuccessMsg } from '../components/untils/notification/notification'
 
 const initialState = {
   email: '',
@@ -13,23 +14,45 @@ const Login = () => {
   const [user, setUser] = useState(initialState)
 
   const {email, password, err, success} = user
+  
+  const handlePressInput = i =>{
+    const{name, value} = i.target
+    setUser({...user, [name]:value, err:'', success:''})
+  }
+
+  const handleSubmitted = async i => {
+    i.preventDefault()
+    try {
+      const res = await axios.post('/user/login', {email, password})
+      setUser({...user, err: '', success: res.data.msg})
+
+      localStorage.setItem('firstLogin', true)
+
+    } catch (err) {
+      err.response.data.msg && 
+      setUser({...user, err: err.response.data.msg, success:''})
+      
+    }
+  }
 
     return (
       <div className="pages_login">
         <h2>Login</h2>
+        {err && showErrMsg(err)}
+        {success && showSuccessMsg(success)}
 
-        <form>
+        <form onSubmit={handleSubmitted}>
 
           <div>
             <label htmlFor="email">Email Address</label>
             <input type="text" placeholder="Enter Your Email" id="email"
-                  value={email} name="email"/>
+                  value={email} name="email" onChange={handlePressInput}/>
           </div>
 
           <div>
             <label htmlFor="password">Password</label>
             <input type="password" placeholder="Enter Your Password" id="password"
-                  value={password} name="password"/>
+                  value={password} name="password" onChange={handlePressInput}/>
           </div>
 
           <div className="row">
