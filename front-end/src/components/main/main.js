8e99feb8158faa14ac29241f741ad3ba99d1f9e0
Huynh Mac Tu Khoa <huynhmactukhoa311@MacBook-Pro-cua-Huynh.local>
2022-02-18 13:05:29 +0700

    Activate Email when Success Register Account

--- front-end/src/components/main/main.js
+++ front-end/src/components/main/main.js
@@ -2,12 +2,15 @@ import React from "react";
 import {Routes, Route} from "react-router-dom";
 import Login from '../../pages/login'
 import Register from '../../pages/register'
+import ActivateEmail from '../../pages/activateEmail';
 
 const Main = () => {
     return(
         <Routes>
                 <Route exact path="/login" element={<Login/>} />
                 <Route exact path="/register" element={<Register/>} />
+                <Route exact path="/user/activate/:activation_token" element={<ActivateEmail/>} />
+
         </Routes>
     )
 }
---
+++ front-end/src/pages/activateEmail.js
@@ -0,0 +1,35 @@
+import React, {useState, useEffect} from 'react'
+import {useParams} from "react-router-dom"
+import axios from 'axios'
+import { showErrMsg, showSuccessMsg } from '../components/untils/notification/notification'
+
+const ActivateEmail = () => {
+    const {activation_token} = useParams()
+    const [err, setErr] = useState('')
+    const [success, setSuccess] = useState('')
+
+    useEffect(() => {
+        if(activation_token){
+            const activateEmail = async () =>{
+                 try {
+                     const res = await axios.post('/user/activation', {activation_token})
+                     setSuccess(res.data.msg)
+                     
+                 } catch (err) {
+                     err.response.data.msg && setErr()
+                     
+                 }
+            }
+            activateEmail()
+        }
+    },[activation_token])
+
+    return (
+        <div className='page_active'>
+            {err && showErrMsg(err)}
+            {success && showSuccessMsg(success)}
+        </div>
+  )
+}
+
+export default ActivateEmail
