f82ec9b1677112b475c22a071b76d6487e4d3903
Huynh Mac Tu Khoa <huynhmactukhoa311@MacBook-Pro-cua-Huynh.local>
2022-02-26 11:22:00 +0700

    fixed setting-ACTIONS redux err 2

--- front-end/src/redux/reducers/postsReducer.js
+++ front-end/src/redux/reducers/postsReducer.js
@@ -1,16 +1,16 @@
-import { FETCH_ALL, CREATE, UPDATE, DELETE, LIKE } from '../actions/index';
+import ACTIONS from '../actions/index';
 
 export default (posts = [], action) => {
   switch (action.type) {
-    case FETCH_ALL:
+    case ACTIONS.FETCH_ALL:
       return action.payload;
-    case LIKE:
+    case ACTIONS.LIKE:
       return posts.map((post) => (post._id === action.payload._id ? action.payload : post));
-    case CREATE:
+    case ACTIONS.CREATE:
       return [...posts, action.payload];
-    case UPDATE:
+    case ACTIONS.UPDATE:
       return posts.map((post) => (post._id === action.payload._id ? action.payload : post));
-    case DELETE:
+    case ACTIONS.DELETE:
       return posts.filter((post) => post._id !== action.payload);
     default:
       return posts;
