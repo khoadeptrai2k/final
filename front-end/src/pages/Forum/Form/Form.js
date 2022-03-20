73f27175bffb833445e0e4db739630bb50540048
Huynh Mac Tu Khoa <huynhmactukhoa311@MacBook-Pro-cua-Huynh.local>
2022-03-20 14:06:11 +0700

    change form basic -> form modal type

--- front-end/src/pages/Forum/Form/Form.js
+++ front-end/src/pages/Forum/Form/Form.js
@@ -1,5 +1,8 @@
 import React, {useState} from 'react';
+import 'boxicons';
 import { useSelector, useDispatch } from 'react-redux';
+import { ACTIONS } from '../../../redux/actions/index';
+
 
 const Form = () => {
   const {auth} = useSelector(state => state)
@@ -7,31 +10,39 @@ const Form = () => {
   const [data, setData] = useState('')
 
   return (
-    <div>
+    <div className='form_modal'>
       <form>
-        <div className='form_modal'></div>
+        
         <div className='form_header'>
+          <h2 className='m-0'>Create New Post </h2>
+          <span onClick={() => dispatch({
+            type: ACTIONS.STATUS, payload: false
+          })}>
+            &times;
+          </span>
         </div>
 
         <div className='form_body'>
+
           <textarea name="title" value={data}
           placeholder={`${auth.userHeader.name}, What are you sharing?`}
           onChange={e => setData(e.target.value)}
-        />
+          />
 
           <div className='input_images'>
-            <i className='fas fa-camera'/>
-
+              <box-icon name='camera' type='solid' ></box-icon>
             <div className='file_upload'>
-              <i className='fas fa-image'/>
-              <input type="file" name="file" id="file" multiple accept='image/*'/>
+              <box-icon type='solid' name='file-image'></box-icon>              <input type="file" name="file" id="file" multiple accept='image/*'/>
             </div>
           </div>
-          
+
         </div>
 
 
-        <div className='form_modal'></div>
+        <div className='form_footer my-5'>
+          <button className='btn btn-primary w-100'>Post</button>
+        </div>
+
 
       </form>
 
